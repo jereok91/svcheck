bd7984c01f2d19ce79ec57a802b022f858d61e29
jereok91 <jereok91@gmail..com>
2025-06-04 17:36:28 -0500

    fixed congfig in win

--- src/utilities/config.rs
+++ src/utilities/config.rs
@@ -11,23 +11,40 @@ pub struct Config {
 pub struct Ia {
     pub api_key: String,
     pub api_url: String,
-    pub pront_ia: String, 
+    pub pront_ia: String,
 }
 
 impl Config {
     pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
-        let config_path = dirs::config_dir()
-            .unwrap_or_else(|| PathBuf::from("."))
-            .join("svcheck/config.toml");
-        println!("{}",&config_path.to_str().unwrap());
+        #[cfg(not(windows))]
+        {
+            let config_path = dirs::config_dir()
+                .unwrap_or_else(|| PathBuf::from("."))
+                .join("svcheck")
+                .join("config.toml");
+            println!("{}", &config_path.to_str().unwrap());
 
-        if !config_path.exists() {
-            return Err("❌ Archivo de configuración no encontrado.".into());
+            if !config_path.exists() {
+                return Err("❌ Archivo de configuración no encontrado.".into());
+            }
+
+            let contents = fs::read_to_string(&config_path)?;
+            let config: Config = toml::from_str(&contents)?;
+            Ok(config)
+        }
+        #[cfg(windows)]
+        {
+            let config_path = std::env::current_exe()?.parent().unwrap().to_path_buf().join("config.toml");            
+            println!("{}", &config_path.to_str().unwrap());
+
+            if !config_path.exists() {
+                return Err("❌ Archivo de configuración no encontrado.".into());
+            }
+
+            let contents = fs::read_to_string(&config_path)?;
+            let config: Config = toml::from_str(&contents)?;
+            Ok(config)
         }
 
-        let contents = fs::read_to_string(&config_path)?;
-        let config: Config = toml::from_str(&contents)?;
-        Ok(config)
     }
-
 }
